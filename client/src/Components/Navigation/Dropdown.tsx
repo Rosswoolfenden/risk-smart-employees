f17821a5200127133c96354cee553004b47b167b
Ross Woolfenden <rosswoolfenden@luminalearning.com>
2023-09-19 17:44:35 +0100

    simple top bar for decoration

---
+++ client/src/Components/Navigation/Dropdown.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+type Props = {
+    name: string;
+    dropdownItems: { name: string; url: string; }[];
+}
+
+const Dropdown = ( props: Props ) => {
+    const { name, dropdownItems } =  props;
+
+    return (
+        <div className='dropdown dropdown-hover'>
+            <label tabIndex={0}>
+                { name }
+            </label>
+            <ul className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            {dropdownItems.map(({ name, url }, index) => (  
+                <li key={index}>
+                    <Link to={url}>{name}</Link>
+                </li>
+            ))}
+            </ul>
+        </div>
+    )
+}
+
+
+export default Dropdown;
---
+++ client/src/Components/Navigation/Topbar.tsx
@@ -0,0 +1,24 @@
+
+
+// Just for decoration purposes
+const TopBar = () => {
+    return (
+        <nav className="w-full bg-gray-800 shadow">
+            <div className="ml-12">
+                <div>
+                    <div className="flex  py-3 md:py-5 md:block">
+                        <a href="#">
+                            <div>
+                                <h1 className="text-3xl text-white font-bold">Reynolds Industry Employee</h1>
+                            </div>
+
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </nav>
+    )
+}
+
+export default TopBar;
